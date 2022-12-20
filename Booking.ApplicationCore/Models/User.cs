7c91f770f53d8154e581c70fc37f2940c6ea0501
z.terehov <zaharkuli67@gmail.com>
2022-12-20 13:30:36 +0300

    added model user

---
+++ Booking.ApplicationCore/Models/User.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Booking.ApplicationCore.Models
+{
+    internal class User
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public decimal NumberPhone { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+    }
+}
