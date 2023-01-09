dea04351304e93f8096ff0187a096f68af656a2a
z.terehov <zaharkuli67@gmail.com>
2023-01-09 15:28:39 +0300

    added IAccountService

---
+++ Booking.Web/Interfaces/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using Booking.Web.Models;
+using System.Security.Claims;
+
+namespace Booking.Web.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<ClaimsIdentity> Register(RegisterViewModel model);
+
+        //Task<ClaimsIdentity> Login(LoginViewModel model);
+
+        //Task<bool> ChangePassword(ChangePasswordViewModel model);
+    }
+}
