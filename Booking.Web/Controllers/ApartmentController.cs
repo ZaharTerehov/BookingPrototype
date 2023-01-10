c4d2dc8eff64979c3e948ff6ec209e3e5af629f7
DzmitryHalubovich <buxarik493@gmail.com>
2023-01-10 19:47:01 +0300

    Add main view

---
+++ Booking.Web/Controllers/ApartmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.Web.Controllers
+{
+    public class ApartmentController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return  View();
+        }
+    }
+}
