15ecf42a909c4e0feef0ff6b4eeb00ec5ebea73e
z.terehov <zaharkuli67@gmail.com>
2023-01-14 23:30:48 +0300

    fix IAccountService

---
+++ Booking.ApplicationCore/Interfaces/IAccountService.cs
@@ -0,0 +1,20 @@
+﻿using Booking.ApplicationCore.Models;
+using Booking.ApplicationCore.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.ApplicationCore.Interfaces
+{
+    public interface IAccountService
+    {
+        public Task<BaseResponse<ClaimsIdentity>> Register(User model);
+
+        //Task<BaseResponse<ClaimsIdentity>> Login(LoginViewModel model);
+
+        //Task<BaseResponse<bool>> ChangePassword(ChangePasswordViewModel model);
+    }
+}
