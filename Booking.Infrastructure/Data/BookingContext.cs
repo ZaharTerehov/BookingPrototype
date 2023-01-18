7ff7e3ce79d55b4179ff989deb065be33a30ce48
z.terehov <zaharkuli67@gmail.com>
2023-01-18 10:05:50 +0300

    added user in data context

--- Booking.Infrastructure/Data/BookingContext.cs
+++ Booking.Infrastructure/Data/BookingContext.cs
@@ -15,6 +15,8 @@ namespace Booking.Infrastructure.Data
         public DbSet<City> Cities { get; set; }
         public DbSet<Apartment> Apartments { get; set; }
 
+        public DbSet<User> Users { get; set; }
+
         public BookingContext(DbContextOptions<BookingContext> options) : base(options) { }
 
         internal Task SaveChangesAsync(IEnumerable<Apartment> enumerable)
