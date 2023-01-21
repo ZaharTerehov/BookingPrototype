e5165232be557ffeaf2562de233ca337f046b244
z.terehov <zaharkuli67@gmail.com>
2023-01-21 14:12:43 +0300

    added LoginViewModel

---
+++ Booking.Web/Models/Account/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Booking.Web.Models;
+
+public class LoginViewModel 
+{
+    [Required(ErrorMessage = "Enter your name")]
+    [MaxLength(20, ErrorMessage = "")]
+    [MinLength(20, ErrorMessage = "")]
+    public string Login { get; set; }
+    
+    [Required(ErrorMessage = "Enter your password")]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+}
