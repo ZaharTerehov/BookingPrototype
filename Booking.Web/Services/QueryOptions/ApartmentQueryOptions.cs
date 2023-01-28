bbfebb8d7f884e9a190a797d2bb895cf050f1903
Aliaksei Matskevich <mats2@tut.by>
2023-01-28 22:35:37 +0300

    Add Address and PeopleNumber to ApartmentViews

--- Booking.Web/Services/QueryOptions/ApartmentQueryOptions.cs
+++ Booking.Web/Services/QueryOptions/ApartmentQueryOptions.cs
@@ -1,6 +1,7 @@
 ﻿using Booking.ApplicationCore.Constants;
 using Booking.ApplicationCore.Enums;
 using Booking.ApplicationCore.QueryOptions;
+using System.ComponentModel.DataAnnotations;
 
 namespace Booking.Web.Services.QueryOptions
 {
@@ -9,9 +10,12 @@ namespace Booking.Web.Services.QueryOptions
         public PageOptions PageOptions { get; set; }
         public int? ApartmentTypeFilterApplied { get; set; }
         public int? CityFilterApplied { get; set; }
+        [Range(1,10)]
+        public int? NeedPeopleNumber { get; set; }
 
         public ApartmentQueryOptions()
         {
+            NeedPeopleNumber = 1;
             PageOptions = new PageOptions(0, 1, ApplicationConstants.ApartmentsPageSize);
         }
     }
