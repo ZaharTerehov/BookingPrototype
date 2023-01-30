99e6820a9be6240a080ed069ce64ed9682a0b763
z.terehov <zaharkuli67@gmail.com>
2023-01-30 22:27:48 +0300

    added JwtOptions

---
+++ Booking.Web/Services/Account/JwtOptions.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Booking.Web.Services.Account
+{
+    public sealed class JwtOptions
+    {
+		public string SigningKey { get; init; } 
+        public string Issuer { get; init; }
+        public string Audience { get; init; }
+        public int TokenExpiryInMinutes { get; init; }
+
+        public JwtOptions(string signingKey, string issuer, string audience, int tokenExpiryInMinutes)
+        {
+            SigningKey = signingKey;
+            Issuer= issuer;
+            Audience= audience;
+            TokenExpiryInMinutes = tokenExpiryInMinutes;
+        }
+    }
+}
