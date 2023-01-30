396c8c8e574c7bc5f317a3d8909652cc89c52f6b
Aliaksei Matskevich <mats2@tut.by>
2023-01-30 10:53:04 +0300

    Add Search apartment by custom text

--- Booking.ApplicationCore/Constants/ApplicationConstants.cs
+++ Booking.ApplicationCore/Constants/ApplicationConstants.cs
@@ -10,7 +10,8 @@ namespace Booking.ApplicationCore.Constants
     public static class ApplicationConstants
     {
         public const PageSize ApartmentsPageSize = PageSize.TwoElements;
-
+        public const int MinPeopleNumber = 1;
+        public const int MaxPeopleNumber = 10;
 
     }
 }
