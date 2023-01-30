7f4a920c0f1eaba572072813076ca09401fc2f3c
z.terehov <zaharkuli67@gmail.com>
2023-01-30 22:25:38 +0300

    fix IAccountServiceViewModelService

---
+++ Booking.Web/Interfaces/Account/IAccountServiceViewModelService.cs
@@ -0,0 +1,12 @@
+﻿using Booking.ApplicationCore.Response;
+using Booking.Web.Models;
+
+namespace Booking.Web.Interfaces
+{
+    public interface IAccountServiceViewModelService
+    {
+        ValueTask<BaseResponse> Register(RegisterViewModel model);
+
+		ValueTask<BaseResponse> Login(LoginViewModel model);
+    }
+}
