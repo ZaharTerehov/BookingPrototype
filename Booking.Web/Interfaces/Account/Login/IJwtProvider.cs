b81804d5a015a444fcc9217d0c576719e638f998
z.terehov <zaharkuli67@gmail.com>
2023-01-30 22:24:58 +0300

    added interface IJwtProvider

---
+++ Booking.Web/Interfaces/Account/Login/IJwtProvider.cs
@@ -0,0 +1,9 @@
+﻿using Booking.ApplicationCore.Models;
+using Booking.Web.Services.Account;
+
+namespace Booking.Web.Interfaces.Login;
+
+public interface IJwtProvider
+{
+	JwtTokenResult Generate(User user);
+}
