6eb30506eeac417e81253026c9635ae23ccac91c
z.terehov <zaharkuli67@gmail.com>
2023-02-09 14:14:03 +0300

    added the ability for the user to update data and refresh token

--- Booking.ApplicationCore/Models/User.cs
+++ Booking.ApplicationCore/Models/User.cs
@@ -19,5 +19,55 @@ namespace Booking.ApplicationCore.Models
         public DateTime RegistrationDate { get; set; } = DateTime.Now;
 
         public Role Role { get; set; }
+
+        public string RefreshToken { get; set; }
+
+        public DateTime RefreshTokenExpiryInMinutes { get; set; }
+
+        public void UpdateDetails(UserDetails details)
+        {
+            Email = details.Email;
+            Password = details.Password;
+            Name = details.Name;
+            Surname = details.Surname;
+            NumberPhone = details.NumberPhone;
+            DateOfBirth = details.DateOfBirth;
+        }
+
+        public void UpdateRefreshToken(string refreshToken, DateTime refreshTokenExpiryInMinutes)
+        {
+            RefreshToken = refreshToken;
+            RefreshTokenExpiryInMinutes = refreshTokenExpiryInMinutes;
+        }
+
+        public readonly record struct UserDetails
+        {
+            public string Email { get; }
+
+            public string Password { get; }
+
+            public string Name { get; }
+
+            public string Surname { get; }
+
+            public decimal NumberPhone { get; }
+
+            public DateTime DateOfBirth { get; }
+
+            public string RefreshToken { get; }
+
+            public DateTime RefreshTokenExpiryInMinutes { get; }
+
+            public UserDetails(string email, string password, string name, string surname,
+                decimal numberPhone, DateTime dateOfBirth)
+            {
+                Email = email;
+                Password = password;
+                Name = name;
+                Surname = surname;
+                NumberPhone = numberPhone;
+                DateOfBirth = dateOfBirth;
+            }
+        }
     }
 }
